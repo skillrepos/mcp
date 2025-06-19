777cf59c50104bf70c703fee9233d2dc8ed53e01
Tech Skills Transformations LLC <82771267+techupskills@users.noreply.github.com>
2025-06-19 01:34:42 +0000

    updating startup script

--- lab2/start-inspector.sh
+++ lab2/start-inspector.sh
@@ -1,9 +1,16 @@
+# Kill processes on port 6274
+kill -9 $(lsof -t -i:6274) 2>/dev/null
+
+# Kill processes on port 6277
+kill -9 $(lsof -t -i:6277) 2>/dev/null
+
 export DANGEROUSLY_OMIT_AUTH=true
-export MCP_PROXY_FULL_ADDRESS=https://${CODESPACE_NAME}-6277.app.github.dev
-export ALLOWED_ORIGINS=https://${CODESPACE_NAME}-6274.app.github.dev,https://${CODESPACE_NAME}-6277.app.github.dev
-( 
-    sleep 15
-    gh codespace ports visibility --codespace $CODESPACE_NAME 8000:public 6274:public 6277:public 8000:public
-    echo Inspector is available at https://${CODESPACE_NAME}-6274.app.github.dev
-) &
-npx --yes @modelcontextprotocol/inspector   --config mcp.json   --server codespace
+export MCP_PROXY_FULL_ADDRESS="https://${CODESPACE_NAME}-6277.app.github.dev"
+export ALLOWED_ORIGINS="https://${CODESPACE_NAME}-6274.app.github.dev,https://${CODESPACE_NAME}-6277.app.github.dev"
+
+echo URL= "https://${CODESPACE_NAME}-6274.app.github.dev/?transport=streamable-http&serverUrl=https://${CODESPACE_NAME}-8000.app.github.dev/mcp/&MCP_PROXY_FULL_ADDRESS=https://${CODESPACE_NAME}-6277.app.github.dev"
+
+npx --yes @modelcontextprotocol/inspector &
+
+sleep 7
+gh codespace ports visibility --codespace $CODESPACE_NAME 8000:public 6274:public 6277:public
