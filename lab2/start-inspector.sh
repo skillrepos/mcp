31976ff88c1daa6578b61ba98fa6a54c54448114
Tech Skills Transformations LLC <82771267+techupskills@users.noreply.github.com>
2025-06-19 02:19:52 +0000

    update script

--- lab2/start-inspector.sh
+++ lab2/start-inspector.sh
@@ -8,9 +8,12 @@ export DANGEROUSLY_OMIT_AUTH=true
 export MCP_PROXY_FULL_ADDRESS="https://${CODESPACE_NAME}-6277.app.github.dev"
 export ALLOWED_ORIGINS="https://${CODESPACE_NAME}-6274.app.github.dev,https://${CODESPACE_NAME}-6277.app.github.dev"
 
+echo "\n\n"
 echo URL= "https://${CODESPACE_NAME}-6274.app.github.dev/?transport=streamable-http&serverUrl=https://${CODESPACE_NAME}-8000.app.github.dev/mcp/&MCP_PROXY_FULL_ADDRESS=https://${CODESPACE_NAME}-6277.app.github.dev"
+echo "\n\n"
 
-npx --yes @modelcontextprotocol/inspector &
 
-sleep 7
-gh codespace ports visibility --codespace $CODESPACE_NAME 8000:public 6274:public 6277:public
+npx --yes @modelcontextprotocol/inspector 
+
+# source start-inspector.sh
+# gh codespace ports visibility --codespace $CODESPACE_NAME 8000:public 6274:public 6277:public
