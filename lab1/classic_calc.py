8d0d8487b8f09d207739d59f038dbb42efc69629
Tech Skills Transformations LLC <82771267+techupskills@users.noreply.github.com>
2025-07-07 17:25:45 +0000

    updates for lab 1

--- lab1/classic_calc.py
+++
@@ -1,6 +0,0 @@
-import requests, urllib.parse, sys
-
-expr = urllib.parse.quote_plus("12*8")
-url  = f"https://api.mathjs.org/v4/?expr={expr}"
-print("Calling:", url)
-print("Result :", requests.get(url, timeout=10).text)
